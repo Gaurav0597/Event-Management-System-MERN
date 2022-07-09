657af4db766c30fa9acd4ab02afa35d785f4d317
GauravPetkar <gauravpetkar28@gmail.com>
2022-07-09 12:11:54 +0530

    All folder structure done,Some Part of NavBar is done

---
+++ Event_Management_System/src/Components/IndustryTalk/IndustryTalk.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IndustryTalk = () => {
+  return (
+    <div>IndustryTalk</div>
+  )
+}
+
+export default IndustryTalk
---
+++ Event_Management_System/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const Navbar = () => {
+  const links = [
+    { name: 'Home', link: '/' },
+    { name: 'Schedule', link: 'schedule' },
+    { name: 'Practice', link: 'practice' },
+    { name: 'Events', link: 'events' },
+    { name: 'Industry Talk', link: 'industry-talk' },
+  ]
+
+  return (
+    <div className="py-2 flex justify-between items-center px-8 z-10">
+      <div className="text-red-500 w-1/6">
+        {/* imagediv */}
+        imageDiv
+      </div>
+      <div className="flex justify-between items-center w-4/6">
+        {/* pagesDiv */}
+        {links.map((item) => {
+          return (
+            
+              <NavLink
+                  to={item.link}
+                className={({ isActive }) => {
+                  return ` text-gray-200 border-b border-purple-500 tracking-widest p-2 px-8 transition hover:bg-purple-400 font-bold hover:scale-125 transform ${
+                    isActive ? 'bg-purple-600' : ''
+                  }`
+                }}
+            
+              >
+                {item.name}
+              </NavLink>
+            
+          )
+        })}
+      </div>
+      <div className="w-1/6 items-center">
+        {/* login/logout div */}
+        LOginDiv
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
